129232262bb41608b4a017c36e5ad8a6becc68ce
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-05 18:59:25 -0400

    modify home page and categories files

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,7 @@ const Home = () => (
   <div className="home-page">
     <h2>Welcome to our page</h2>
     <p>
-      This is my first React SPA project,
-      if you click &quot;calculator&quot; on the header,
-      then you will see a calculator similar to Apple&apos;s design.
-      And you can see a random quote about computer if you click &quot;quote&quot;.
+      This is my Bookstore building with React.
     </p>
   </div>
 );
---
+++ src/components/Categories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Categories = () => <h1>Welcome to the Categories Page!</h1>;
+
+export default Categories;
