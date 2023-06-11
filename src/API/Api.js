6205a1ac44466c3c66dc9a4022e45377d123bb74
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-11 18:44:04 -0400

    install axios and revise api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,56 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const QuoteDisplay = () => {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+const API_BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/nT06772jNHm46u8F57PQ'; 
 
-  useEffect(() => {
-    const fetchQuote = async () => {
-      try {
-        const response = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/nT06772jNHm46u8F57PQ');
-        if (!response.ok) {
-          throw new Error('Failed to fetch quote');
-        }
-
-        const data = await response.json();
-        const randomQuote = data[0].quote;
-        const randomAuthor = data[0].author;
-        setQuote(randomQuote);
-        setAuthor(randomAuthor);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false)
-      }
-    };
-
-    fetchQuote();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return (
-      <div>
-        Error:
-        {error}
-      </div>
-    );
-  }
-
-  return (
-    <div className="quote">
-      {' '}
-      {quote}
-      <br />
-      {' -'}
-      {author}
-    </div>
-  );
+// Fetch all books from the API
+export const fetchBooks = async () => {
+  const response = await axios.get(`${API_BASE_URL}/books`);
+  return response.data;
 };
 
-export default QuoteDisplay;
+// Add a book to the API
+export const addBook = async (book) => {
+  const response = await axios.post(`${API_BASE_URL}/books`, book);
+  return response.data;
+};
+
+// Remove a book from the API
+export const removeBook = async (bookId) => {
+  await axios.delete(`${API_BASE_URL}/books/${bookId}`);
+};
